2009d1171d8caf76e8dc999acfe96209a615a42b
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-06-14 17:19:39 +0600

    admin role api added

--- index.js
+++ index.js
@@ -32,6 +32,44 @@ async function run() {
     const newsCollection = client.db('newsPaperDB').collection('news');
     const userCollection = client.db('newsPaperDB').collection('users');
 
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+
+
     // for showind all data in home
     app.get('/news', async (req, res) => {
       const result = await newsCollection.find().toArray();
@@ -55,6 +93,19 @@ async function run() {
       res.send(result);
     });
 
+    // dashboard related apies
+    /* for making admin */
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
 
 
     // for adding data from add article post method
